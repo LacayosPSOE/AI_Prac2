161e8051ceaac1e46f50a3911bbf11706bcf3d38
VoltorH <130765229+VoltorH@users.noreply.github.com>
2025-03-04 13:05:09 +0100

    Cambio de numeros lol

--- Assets/Practica/Scripts/SKELETON_Blackboard.cs
+++ Assets/Practica/Scripts/SKELETON_Blackboard.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class SKELETON_Blackboard : MonoBehaviour
 {
-  public float scareRange = 800f;
+  public float scareRange = 50f;
   public float timer = 0f;
   public GameObject paladin;
   public GameObject guide;
 }
+                                     
