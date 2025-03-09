5ef9f4e0b3289309bb9a09274c9485dc1be52b7c
joeljoanlopez <joeljoanlopez@gmail.com>
2025-03-09 14:20:30 +0100

    Add GUO and change state/transition namings to be more descriptive

--- Assets/Practica/Scripts/FSM_Guide.cs
+++ Assets/Practica/Scripts/FSM_Guide.cs
@@ -1,7 +1,7 @@
 using FSMs;
 using UnityEngine;
 using Steerings;
-using Pathfinding;  
+using Pathfinding;
 using System.Threading;
 
 [CreateAssetMenu(fileName = "FSM_Guide", menuName = "Finite State Machines/FSM_Guide", order = 1)]
@@ -10,98 +10,85 @@ public class FSM_Guide : FiniteStateMachine
     private GUIDE_Blackboard blackboard;
     private PathFeeder pathFeeder;
     private GameObject wanderPoint;
-    private GraphUpdateScene graphUpdateScene;  
+    private GraphUpdateScene graphUpdateScene;
 
     public override void OnEnter()
     {
         blackboard = GetComponent<GUIDE_Blackboard>();
         pathFeeder = GetComponent<PathFeeder>();
-        graphUpdateScene = GetComponent<GraphUpdateScene>(); 
         base.OnEnter();
     }
 
     public override void OnExit()
     {
+        base.DisableAllSteerings();
         base.OnExit();
     }
 
     public override void OnConstruction()
     {
         State wandering = new State("Wandering",
-            () => {
+            () =>
+            {
                 wanderPoint = SensingUtils.FindRandomInstanceWithinRadius(gameObject, "POINT", 1000f);
                 pathFeeder.target = wanderPoint;
-                ApplyGraphUpdate();
             },
             () => { },
             () => { }
         );
-
-        State foundCultist = new State("Found_Cultist",
-            () => { 
+        State stayWithCultist = new State("Found_Cultist",
+            () =>
+            {
                 pathFeeder.target = blackboard.cultist;
-                ApplyGraphUpdate();  
             },
             () => { },
             () => { }
         );
-
         State runAway = new State("Run_Away",
-            () => {
+            () =>
+            {
                 blackboard.timer = 0f;
-                Vector3 fleeDirection = (gameObject.transform.position - blackboard.skeleton.transform.position).normalized;
+                Vector3 fleeDirection =
+                    (gameObject.transform.position - blackboard.skeleton.transform.position).normalized;
                 Vector3 fleePosition = gameObject.transform.position + fleeDirection * blackboard.runDistance;
 
                 GameObject fleePoint = new GameObject("FleePoint");
                 fleePoint.transform.position = fleePosition;
 
                 pathFeeder.target = fleePoint;
-
-                ApplyGraphUpdate();
             },
-            () => {
-                blackboard.timer += Time.deltaTime;
-            },
-            () => {
-                blackboard.timer = 0f;
-            }
+            () => { blackboard.timer += Time.deltaTime; },
+            () => { blackboard.timer = 0f; }
         );
 
         Transition cultistWithinRange = new Transition("Cultist_Within_Range",
             () => { return SensingUtils.DistanceToTarget(gameObject, blackboard.cultist) <= blackboard.cultistRange; },
             () => { }
         );
-
         Transition SkeletonTooClose = new Transition("Skeleton_Too_Close",
             () => { return SensingUtils.DistanceToTarget(gameObject, blackboard.skeleton) <= blackboard.runRange; },
             () => { }
         );
-
         Transition checkPointReached = new Transition("CheckPoint_Reached",
-            () => { return SensingUtils.DistanceToTarget(gameObject, wanderPoint) <= blackboard.reachedRange || SensingUtils.DistanceToTarget(gameObject, blackboard.cultist) <= blackboard.reachedRange; },
+            () =>
+            {
+                return SensingUtils.DistanceToTarget(gameObject, wanderPoint) <= blackboard.reachedRange ||
+                       SensingUtils.DistanceToTarget(gameObject, blackboard.cultist) <= blackboard.reachedRange;
+            },
             () => { }
         );
-
         Transition tired = new Transition("Tired",
             () => { return blackboard.timer >= 6f; },
             () => { }
         );
 
-        AddStates(wandering, foundCultist, runAway);
+        AddStates(wandering, stayWithCultist, runAway);
         AddTransition(wandering, checkPointReached, wandering);
-        AddTransition(wandering, cultistWithinRange, foundCultist);
+        AddTransition(wandering, cultistWithinRange, stayWithCultist);
         AddTransition(wandering, SkeletonTooClose, runAway);
-        AddTransition(foundCultist, SkeletonTooClose, runAway);
+        AddTransition(stayWithCultist, SkeletonTooClose, runAway);
         AddTransition(runAway, tired, wandering);
 
         initialState = wandering;
-    }
-
-    private void ApplyGraphUpdate()
-    {
-        if (graphUpdateScene != null)
-        {
-            graphUpdateScene.Apply();  
-        }
     }
 }
--- Assets/Practica/Scripts/FSM_Skeleton_Scare.cs
+++ Assets/Practica/Scripts/FSM_Skeleton_Scare.cs
@@ -5,7 +5,6 @@ using Steerings;
 [CreateAssetMenu(fileName = "FSM_Skeleton_Scare", menuName = "Finite State Machines/FSM_Skeleton_Scare", order = 1)]
 public class FSM_Skeleton_Scare : FiniteStateMachine
 {
-
     private SKELETON_Blackboard blackboard;
     private PathFeeder pathFeeder;
 
@@ -13,43 +12,53 @@ public class FSM_Skeleton_Scare : FiniteStateMachine
     {
         blackboard = GetComponent<SKELETON_Blackboard>();
         pathFeeder = GetComponent<PathFeeder>();
-        base.OnEnter(); 
+        base.OnEnter();
     }
 
     public override void OnExit()
     {
+        base.DisableAllSteerings();
         base.OnExit();
     }
 
     public override void OnConstruction()
     {
-
         State chasingPaladin = new State("Chasing_Paladin",
-            () => { pathFeeder.target = blackboard.paladin; }, 
-            () => { if (blackboard.timer > 0) { blackboard.timer -= Time.deltaTime; } else { } }, 
-            () => { }   
+            () => { pathFeeder.target = blackboard.paladin; },
+            () =>
+            {
+                if (blackboard.timer > 0)
+                {
+                    blackboard.timer -= Time.deltaTime;
+                }
+                else
+                {
+                }
+            },
+            () => { }
         );
-
         State scareGuide = new State("Scare_Guide",
-            () => { pathFeeder.target = blackboard.guide; }, 
-            () => { blackboard.timer += Time.deltaTime; }, 
-            () => { }  
+            () => { pathFeeder.target = blackboard.guide; },
+            () => { blackboard.timer += Time.deltaTime; },
+            () => { }
         );
-
-
-        Transition chaseGuide = new Transition("Chase_Guide",
-            () => { return blackboard.timer <= 0 && SensingUtils.DistanceToTarget(gameObject, blackboard.guide) <= blackboard.scareRange;  }, 
-            () => { }  
+        
+        Transition guideClose = new Transition("Chase_Guide",
+            () =>
+            {
+                return blackboard.timer <= 0 &&
+                       SensingUtils.DistanceToTarget(gameObject, blackboard.guide) <= blackboard.scareRange;
+            },
+            () => { }
         );
-                Transition quitChasingGuide = new Transition("Quit_Chasing_Guide",
-            () => {return blackboard.timer >= 5;},
-            () => { }  
+        Transition quitChasingGuide = new Transition("Quit_Chasing_Guide",
+            () => { return blackboard.timer >= 5; },
+            () => { }
         );
         AddStates(chasingPaladin, scareGuide);
-        AddTransition(chasingPaladin, chaseGuide, scareGuide);
+        AddTransition(chasingPaladin, guideClose, scareGuide);
         AddTransition(scareGuide, quitChasingGuide, chasingPaladin);
 
-         initialState = chasingPaladin;
-
+        initialState = chasingPaladin;
     }
 }
